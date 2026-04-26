e928e2ebde26111003fe774b3854315cb2b2ecab
OMW.DEVELOPER <omwadhane.dev@gmail.com>
2026-04-27 01:48:56 +0530

    Delete S2R2-Final-C - Copy directory

--- S2R2-Final-C - Copy/SETUP-DATABASE.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo.
-echo ========================================
-echo   S2R2 IoT Manager - Database Setup
-echo ========================================
-echo.
-node setup-database-new.js
-echo.
-pause
